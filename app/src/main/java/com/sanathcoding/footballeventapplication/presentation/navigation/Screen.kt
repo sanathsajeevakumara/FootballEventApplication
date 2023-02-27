19858b30a26fb42286dab421eadcb6254033b9c4
Sanath <sanath@entrusttitle.com>
2023-02-28 03:07:48 +0530

    SetUp Screen Navigation

--- app/src/main/java/com/sanathcoding/footballeventapplication/presentation/navigation/Screen.kt
+++ app/src/main/java/com/sanathcoding/footballeventapplication/presentation/navigation/Screen.kt
@@ -1,12 +1,14 @@
 package com.sanathcoding.footballeventapplication.presentation.navigation
 
+import com.sanathcoding.footballeventapplication.core.common.FootballValue.HOME_SCREEN
 import com.sanathcoding.footballeventapplication.core.common.FootballValue.MATCH_SCREEN
 import com.sanathcoding.footballeventapplication.core.common.FootballValue.TEAMS_SCREEN
 import com.sanathcoding.footballeventapplication.core.common.FootballValue.TEAM_DETAIL_SCREEN
 
 sealed class Screen(val route: String) {
+
+    object HomeScreen: Screen(HOME_SCREEN)
     object TeamsScreen: Screen(TEAMS_SCREEN)
     object MatchScreen: Screen(MATCH_SCREEN)
-
     object TeamDetailScreen: Screen(TEAM_DETAIL_SCREEN)
 }
