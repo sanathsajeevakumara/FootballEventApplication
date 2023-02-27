5993af1d6d70653e2f1fffc7a98136fbd9a042b3
Sanath <sanath@entrusttitle.com>
2023-02-27 16:35:51 +0530

    Created Teams ViewModel

--- app/src/main/java/com/sanathcoding/footballeventapplication/domain/repository/FootballRepository.kt
+++ app/src/main/java/com/sanathcoding/footballeventapplication/domain/repository/FootballRepository.kt
@@ -3,13 +3,14 @@ package com.sanathcoding.footballeventapplication.domain.repository
 import com.sanathcoding.footballeventapplication.core.common.Resource
 import com.sanathcoding.footballeventapplication.domain.model.match.Match
 import com.sanathcoding.footballeventapplication.domain.model.teams.Team
+import kotlinx.coroutines.flow.Flow
 
 interface FootballRepository {
 
-    suspend fun getTeamData(): Resource<List<Team>>
+    suspend fun getTeamData(): Flow<Resource<List<Team>>>
 
-    suspend fun getMatchData(): Resource<Match>
+    suspend fun getMatchData(): Flow<Resource<Match>>
 
-    suspend fun getMatchByTeamId(id: String): Resource<Match>
+    suspend fun getMatchByTeamId(id: String): Flow<Resource<Match>>
 
 }
