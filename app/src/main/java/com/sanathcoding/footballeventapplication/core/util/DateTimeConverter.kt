14d3067e1a29b352d845a1072d0fdddfc56f8e95
Sanath <sanath@entrusttitle.com>
2023-03-02 11:33:18 +0530

    Updated Date time converters

--- app/src/main/java/com/sanathcoding/footballeventapplication/core/util/DateTimeConverter.kt
+++ app/src/main/java/com/sanathcoding/footballeventapplication/core/util/DateTimeConverter.kt
@@ -3,20 +3,17 @@ package com.sanathcoding.footballeventapplication.core.util
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun dateTimeConverter(date: String): String {
-    val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-    val formatter = SimpleDateFormat("dd MM yyyy HH:mm a", Locale.US)
-    return formatter.format(parser.parse(date))
-}
+val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
+val dateFormatter = SimpleDateFormat("dd-MM-yy", Locale.US)
+val timeFormatter = SimpleDateFormat("HH:mm a", Locale.US)
+var parserDate: Date = Date()
 
 fun dateConverter(date: String): String {
-    val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-    val formatter = SimpleDateFormat("dd-MM-yy", Locale.US)
-    return formatter.format(parser.parse(date))
+    parser.parse(date)?.let { parserDate = it }
+    return dateFormatter.format(parserDate)
 }
 
 fun timeConverter(date: String): String {
-    val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-    val formatter = SimpleDateFormat("HH:mm a", Locale.US)
-    return formatter.format(parser.parse(date))
+    parser.parse(date)?.let { parserDate = it }
+    return timeFormatter.format(parserDate)
 }
